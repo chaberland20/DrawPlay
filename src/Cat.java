62fe5d6775a371d47956ceaf135cee572f8cda29
Ian Radley <iradley@mymail.mines.edu>
2020-09-29 10:48:04 -0600

    Made the mouth more circular and green, also changed text to include a name.

--- src/Cat.java
+++ src/Cat.java
@@ -37,13 +37,13 @@ public class Cat {
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.pink);
-		x = catX + MOUTH_X;
-		y = catY + MOUTH_Y;
+		g2.setColor(Color.green);
+		x = catX + MOUTH_X-2;
+		y = catY + MOUTH_Y-2;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Meow! my name is Sally", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
